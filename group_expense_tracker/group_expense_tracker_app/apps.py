b8d73c68f44edd7047c7e1a3829e4c408b5d05a2
Němec, Pavel <pavel.nemec@lyse.no>
2023-11-29 22:20:12 +0100

    django and strawberry installed and configured

---
+++ group_expense_tracker/group_expense_tracker_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GroupExpenseTrackerAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'group_expense_tracker_app'
