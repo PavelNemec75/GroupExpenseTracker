50888eb177ce8315ba20fd6c84ee9f84dad3a02f
Němec, Pavel <pavel.nemec@lyse.no>
2023-12-02 21:50:01 +0100

    add_participant_to_event added

--- README.md
+++ README.md
@@ -12,4 +12,7 @@ verze neresi:
 - autorizaci
 - lokalizaci
 - nelze smazat participant pokud ma zaznam v event_expense_group
-zde by sla pridat dalsi logika - napr. lze smazat pokud je jeho bilance nulova
+zde by sla pridat dalsi logika - napr. lze smazat pokud je jeho bilance nulova atd.
+- validace emailove adresy
+- nezobrazovat detailni chybove hlasky v produkcnim rezimu
+- komentare v kodu
--- group_expense_tracker/group_expense_tracker_app/schema.py
+++ group_expense_tracker/group_expense_tracker_app/schema.py
@@ -10,6 +10,7 @@ from django.core.exceptions import ObjectDoesNotExist
 
 @strawberry.type
 class Query:
+
     @strawberry.field
     def get_events(self) -> List[EventType]:
         return list(Event.objects.all())
@@ -104,45 +105,61 @@ class Mutation:
     @strawberry.mutation
     def delete_participant(
             self,
-            # participant_id: strawberry.ID,
             participant_id: Optional[strawberry.ID] = None,
             participant_email: Optional[str] = None,
     ) -> bool:
         if not participant_id and not participant_email:
             raise ValueError("Either participant_id or participant_email must be provided.")
 
-        # try:
-        #     participant = Participant.objects.get(participant_id=participant_id)
-        # except ObjectDoesNotExist as err:
-        #     raise ValueError("Participant not found.") from err
-
-        participant = None
+        participant_to_delete = None
 
         try:
             if participant_id:
-                participant = Participant.objects.get(participant_id=participant_id)
+                participant_to_delete = Participant.objects.get(participant_id=participant_id)
             elif participant_email:
-                participant = Participant.objects.get(participant_email=participant_email)
+                participant_to_delete = Participant.objects.get(participant_email=participant_email)
         except ObjectDoesNotExist as err:
             raise ValueError("Participant not found.") from err
 
         expense_group_exists = EventExpenseGroup.objects.filter(
-            event_participant__participant=participant,
+            event_participant__participant=participant_to_delete,
         ).exists()
 
         if expense_group_exists:
             raise ValueError("Cannot delete Participant with associated EventExpenseGroup records.")
 
-        participant.delete()
+        participant_to_delete.delete()
         return True
 
     @strawberry.mutation
-    def add_event_participant(self, event_id: str, participant_id: str) -> EventParticipantType:
-        event = Event.objects.get(event_id=event_id)
-        participant = Participant.objects.get(participant_id=participant_id)
-        event_participant = EventParticipant(event=event, participant=participant)
-        event_participant.save()
-        return event_participant
+    def add_participant_to_event(
+            self,
+            event_id: str,
+            participant_id: str,
+    ) -> EventParticipantType:
+
+        if not Event.objects.filter(event_id=event_id).exists():
+            raise ValueError(f"Event with ID {event_id} does not exist.")
+
+        if not Participant.objects.filter(participant_id=participant_id).exists():
+            raise ValueError(f"Participant with ID {participant_id} does not exist.")
+
+        if EventParticipant.objects.filter(event__event_id=event_id,
+                                           participant__participant_id=participant_id).exists():
+            raise ValueError(
+                f"EventParticipant record with Event ID {event_id} and Participant ID {participant_id} already exists.")
+
+        event_to_add = Event.objects.get(event_id=event_id)
+        participant_to_add = Participant.objects.get(participant_id=participant_id)
+        new_event_participant = EventParticipant(event=event_to_add, participant=participant_to_add)
+        new_event_participant.save()
+
+        return EventParticipantType(
+            event_participant_registered_at=new_event_participant.event_participant_registered_at,
+            event_participant_id=new_event_participant.event_participant_id,
+            participant=participant_to_add,
+            event=event_to_add,
+        )
 
 
 schema = strawberry.Schema(query=Query, mutation=Mutation)
